f2583286940f7c51c1a69485190f77f32759a4d1
dmercado98 <98d.mercado@gmail.com>
2020-09-23 01:48:38 +0100

    Add,subtract, dotproduct done.

--- v3math.c
+++ v3math.c
@@ -2,37 +2,40 @@
 #include "v3math.h"
 // form v3 from a to b
 void v3_from_points(float *dst, float *a, float *b){
-printf("What");
 } 
 void v3_add(float *dst, float *a, float *b){
-printf("What");
+    dst[0] = a[0] + b[0];
+    dst[1] = a[1] + b[1];
+    dst[2] = a[2] + b[2];
 }
 void v3_subtract(float *dst, float *a, float *b){
-printf("What");
+    dst[0] = a[0] - b[0];
+    dst[1] = a[1] - b[1];
+    dst[2] = a[2] - b[2];
 }
 float v3_dot_product(float *a, float *b){
-printf("What");
+    return (a[0] * b[0]) + (a[1]*b[1]) + (a[2]*b[2]);
 }
 void v3_cross_product(float *dst, float *a, float *b){
-printf("What");
+printf("Cross");
 }
 void v3_scale(float *dst, float s){
-printf("What");
+printf("scale");
 }
  // angle between a and b
 void v3_angle(float *dst, float *a, float *b){
-printf("What");
+printf("angle");
 }
 // angle between a and b; no cos-1
 void v3_angle_quick(float *dst, float *a, float *b){
-printf("What");
+printf("quick angle");
 }
 void v3_reflect(float *dst, float *v, float *n){
-printf("What");
+printf("reflect");
 }
 float v3_length(float *a){
-printf("What");
+printf("length");
 }
 void v3_normalize(float *dst, float *a){
-    printf("What");
+    printf("norm");
 }
--- v3test.c
+++ v3test.c
@@ -1,6 +1,21 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "v3math.h"
 int main(void)
 {
-    printf("What");
+    float *testVectA = (float *)malloc(sizeof(float)*3);
+    float *testVectB = (float *)malloc(sizeof(float)*3);
+    float *testVectDest = (float *)malloc(sizeof(float)*3);
+
+    testVectA[0] = 1.234;
+    testVectA[1] = 2.245;
+    testVectA[2] = 0;
+
+    testVectB[0] = 2.245;
+    testVectB[1] = 1.234;
+    testVectB[2] = 5.2;
+
+    v3_subtract(testVectDest,testVectA,testVectB);
+
+    printf("%.6f",v3_dot_product(testVectA,testVectB));
 }
